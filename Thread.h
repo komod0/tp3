9e9b1714f0b67830b510fba0b1593d416b3f7d55
komod0 <juliancrespo15@gmail.com>
2020-05-26 03:59:59 -0300

    Thread class

---
+++ Thread.h
@@ -0,0 +1,24 @@
+#include <thread>
+
+class Thread {
+private:
+  std::thread thread;
+public:
+  Thread();
+
+  virtual ~Thread();
+
+  Thread(const Thread&) = delete;
+
+  Thread& operator=(const Thread&) = delete;
+
+  Thread(Thread&& other);
+
+  Thread& operator=(Thread&& other);
+
+  void start();
+
+  virtual void run() = 0;
+
+  void join();
+};
